5ad4d853c0fc2c74bfe778051e00c0fe9ba88807
Mitchell Kember <mk12360@gmail.com>
2015-10-31 00:51:16 -0400

    Implement print_expr function

--- expr.c
+++ expr.c
@@ -2,7 +2,12 @@
 
 #include "expr.h"
 
+#include <stdio.h>
 #include <stdlib.h>
+
+static const char *special_names[] = {
+	"atom?", "eq?", "car", "cdr", "cons", "add", "sub", "mul", "div"
+};
 
 struct Expression *new_cons(struct Expression *car, struct Expression *cdr) {
 	struct Expression *expr = malloc(sizeof *expr);
@@ -50,6 +55,30 @@ struct Expression *new_special(enum SpecialType type) {
 }
 
 void print_expression(struct Expression *expr) {
+	switch (expr->type) {
+	case E_NULL:
+		fputs("()", stdout);
+		break;
+	case E_CONS:
+		putchar('(');
+		print_expression(expr->cons.car);
+		fputs(" . ", stdout);
+		print_expression(expr->cons.cdr);
+		putchar(')');
+		break;
+	case E_SYMBOL:
+		fputs(expr->symbol.name, stdout);
+		break;
+	case E_NUMBER:
+		printf("%d", expr->number.n);
+		break;
+	case E_LAMBDA:
+		printf("#<%p>", expr->lambda.body);
+		break;
+	case E_SPECIAL:
+		printf("#<%s>", special_names[expr->special.type]);
+		break;
+	}
 }
 
 void free_expression(struct Expression *expr) {
