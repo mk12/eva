8072c1368aae94141ca2a520e4cc51f334f41cae
Mitchell Kember <mk12360@gmail.com>
2015-11-19 11:48:52 -0500

    Implement string interning

--- intern.c
+++ intern.c
@@ -2,14 +2,54 @@
 
 #include "intern.h"
 
-int intern_string(const char *str) {
-	return 0;
+#include <string.h>
+
+struct InternList {
+	const char *string;
+	struct InternList *rest;
+};
+
+#define TABLE_SIZE_BITS 10
+#define TABLE_SIZE 1024
+
+static struct *InternList intern_table[TABLE_SIZE];
+
+unsigned int intern_string(const char *str) {
+	return intern_string_n(str, strlen(str));
 }
 
-int intern_string_n(const char *str, int n) {
-	return 0;
+unsigned int intern_string_n(const char *str, int n) {
+	unsigned int h = 5381;
+	for (int i = 0; i < n; i++) {
+		h = ((h << 5) + h) + str[i];
+	}
+	h %= TABLE_SIZE;
+
+	struct InternList *new_cell = malloc(sizeof *cell);
+	new_cell->string = str;
+	new_cell->rest = NULL;
+
+	int pos = 0;
+	struct InternList *cell = intern_table[h];
+	if (!cell) {
+		intern_table[h] = new_cell;
+	} else {
+		while (cell->rest) {
+			cell = cell->rest;
+			pos++;
+		}
+		cell->rest = new_cell;
+	}
+
+	return (pos << TABLE_SIZE_BITS) | h;
 }
 
-const char *find_string(int id) {
-	return "";
+const char *find_string(unsigned int id) {
+	int h = id & (TABLE_SIZE - 1);
+	int pos = id >> TABLE_SIZE_BITS;
+	struct InternList *cell = intern_table[h];
+	for (int i = 0; i < pos; i++) {
+		cell = cell->rest;
+	}
+	return cell->string;
 }
--- intern.h
+++ intern.h
@@ -4,10 +4,10 @@
 #define INTERN_H
 
 // Interns the string and returns its unique identifier.
-int intern_string(const char *str);
+unsigned int intern_string(const char *str);
 
 // Interns a string of n characters. Does not require a null terminator.
-int intern_string_n(const char *str, int n);
+unsigned int intern_string_n(const char *str, int n);
 
 // Looks up a string by identifier.
 const char *find_string(int id);
