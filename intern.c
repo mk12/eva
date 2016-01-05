9579e11bb67bb2ca641e6a5cd91a6c42cfa4c9ef
Mitchell Kember <mk12360@gmail.com>
2016-01-05 17:14:44 -0500

    Add comments to intern implementation

--- intern.c
+++ intern.c
@@ -22,42 +22,54 @@ InternID intern_string(const char *str) {
 }
 
 InternID intern_string_n(const char *str, int n) {
+	// Calculate the hash value of the string.
 	InternID h = 5381;
 	for (int i = 0; i < n; i++) {
 		h = ((h << 5) + h) + str[i];
 	}
 	h %= TABLE_SIZE;
-	struct Bucket *bucket = table + h;
 
+	// Look up the bucket. Initialize it if it is empty.
+	struct Bucket *bucket = table + h;
 	if (!bucket->strings) {
 		bucket->cap = DEFAULT_BUCKET_SIZE;
 		bucket->len = 0;
 		bucket->strings = malloc(DEFAULT_BUCKET_SIZE * sizeof *bucket->strings);
 	}
 
+	// Check if the same string has already been interned.
 	for (int i = 0; i < bucket->len; i++) {
 		if (strncmp(str, bucket->strings[i], n) == 0) {
+			// If so, return its intern ID.
 			return (i << TABLE_SIZE_BITS) | h;
 		}
 	}
 	
+	// Double the array's capacity if necessary.
 	if (bucket->len >= bucket->cap) {
 		bucket->cap *= 2;
 		bucket->strings = realloc(
 				bucket->strings, bucket->cap * sizeof *bucket->strings);
 	}
 
+	// Copy the string and add a null terminator.
 	char *new_str = malloc(n + 1);
 	strncpy(new_str, str, n);
 	new_str[n] = '\0';
+	// Add the string to the bucket.
 	int pos = bucket->len;
 	bucket->strings[pos] = new_str;
+	// Update the count.
 	bucket->len++;
+
+	// Create an intern ID by combining the position bits and hash bits.
 	return (pos << TABLE_SIZE_BITS) | h;
 }
 
 const char *find_string(InternID id) {
+	// Decode the hash value and the position.
 	int h = id & (TABLE_SIZE - 1);
 	int pos = id >> TABLE_SIZE_BITS;
+	// Look up the interned string.
 	return table[h].strings[pos];
 }
