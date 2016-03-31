1ff679378c9fe819fe813c25c657e459a87044a9
Mitchell Kember <mk12360@gmail.com>
2016-03-31 00:51:21 -0700

    Use int, not ParseErrorType, to allow -1

--- src/parse.h
+++ src/parse.h
@@ -9,14 +9,14 @@
 // ParseResult contains the result of parsing text. The 'expr' field has a
 // meaningful value if and only if 'err_type' is -1.
 struct ParseResult {
-	size_t chars_read;
-	struct Expression expr;
-	enum ParseErrorType err_type;
+	size_t chars_read;      // number of characters read
+	struct Expression expr; // parsed expression
+	int err_type;           // -1 or a ParseErrorType value
 };
 
 // Parses a string as an s-expression of pairs, symbols, and numbers. On
 // success, returns the parse result with 'err_type' set to -1. Otherwise,
-// returns the error type in the parse result.
+// returns a ParseErrorType in the result.
 struct ParseResult parse(const char *text);
 
 #endif
