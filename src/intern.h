18cc86c38ed40e26e73a00e2039346680b363fc9
Mitchell Kember <mk12360@gmail.com>
2016-04-09 17:23:55 -0700

    Revert parts of the revert

--- src/intern.h
+++ src/intern.h
@@ -4,9 +4,10 @@
 #define INTERN_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 // An InternId is a unique identifier for an interned string.
-typedef unsigned int InternId;
+typedef uint32_t InternId;
 
 // Interns the string and returns its unique identifier.
 InternId intern_string(const char *str);
@@ -15,7 +16,8 @@ InternId intern_string(const char *str);
 // the same string has previously been interned, returns the same identifier.
 InternId intern_string_n(const char *str, size_t n);
 
-// Looks up a string by identifier.
+// Looks up a string by identifier. The identifier 'id' must be a value returned
+// from 'intern_string' or 'intern_string_n' (this is not checked).
 const char *find_string(InternId id);
 
 #endif
