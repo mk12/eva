6db045d715de33c6a91e48d7ad1a97adbb7473f9
Mitchell Kember <mk12360@gmail.com>
2016-04-19 00:51:10 -0700

    Fix typo in expr (print macro name, not proc name)

--- src/expr.c
+++ src/expr.c
@@ -370,7 +370,7 @@ void print_expression(struct Expression expr, FILE *stream) {
 		break;
 	case E_STDMACRO:
 		fprintf(stream, "#<macro %s>",
-				stdproc_name_arity[expr.stdproc].name);
+				stdmacro_name_arity[expr.stdproc].name);
 		break;
 	case E_STDPROCEDURE:
 		fprintf(stream, "#<procedure %s>",
